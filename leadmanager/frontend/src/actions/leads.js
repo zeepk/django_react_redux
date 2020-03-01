023c953894b7c1cbf79925d84aa84724da173f79
unknown <mhughes.ipod@gmail.com>
2020-03-01 14:10:03 -0800

    pt 4 done

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { createMessage } from './messages';
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
 
 //had to add these lines to get DELETE_LEAD to not give a 403 error
 //courtesy of https://stackoverflow.com/a/46195212
@@ -24,6 +25,7 @@ export const deleteLead = id => dispatch => {
 	axios
 		.delete(`/api/leads/${id}/`)
 		.then(res => {
+			dispatch(createMessage({ deleteLead: 'Lead Deleted' }));
 			dispatch({
 				type: DELETE_LEAD,
 				payload: id
@@ -37,10 +39,21 @@ export const addLead = lead => dispatch => {
 	axios
 		.post('/api/leads/', lead)
 		.then(res => {
+			dispatch(createMessage({ addLead: 'Lead Added' }));
+
 			dispatch({
 				type: ADD_LEAD,
 				payload: res.data
 			});
 		})
-		.catch(err => console.log(err));
+		.catch(err => {
+			const errors = {
+				msg: err.response.data,
+				status: err.response.status
+			};
+			dispatch({
+				type: GET_ERRORS,
+				payload: errors
+			});
+		});
 };
