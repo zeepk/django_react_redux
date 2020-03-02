5903295f8eca9f38c5ebc1e9166268108c408a57
t710717 <matthew.l.hughes@kp.org>
2020-03-02 11:13:46 -0800

    pt 7 8:50. check for login_success

--- leadmanager/frontend/src/actions/auth.js
+++ leadmanager/frontend/src/actions/auth.js
@@ -1,40 +1,77 @@
-import axios from 'axios';
-import { returnErrors } from './messages';
+import axios from "axios";
+import { returnErrors } from "./messages";
 
-import { USER_LOADED, USER_LOADING, AUTH_ERROR } from './types';
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
+} from "./types";
 
 // check token and load user
 export const loadUser = () => (dispatch, getState) => {
-	// user loading
-	dispatch({ type: USER_LOADING });
+  // user loading
+  dispatch({ type: USER_LOADING });
 
-	// get token from state
-	const token = getState().auth.token;
+  // get token from state
+  const token = getState().auth.token;
 
-	// send headers with request
-	const config = {
-		headers: {
-			'content-type': 'application/json'
-		}
-	};
+  // send headers with request
+  const config = {
+    headers: {
+      "content-type": "application/json"
+    }
+  };
 
-	// if token, add to headers
-	if (token) {
-		config.headers['Authorization'] = `Token ${token}`;
-	}
+  // if token, add to headers
+  if (token) {
+    config.headers["Authorization"] = `Token ${token}`;
+  }
 
-	axios
-		.get('/api/auth/user', config)
-		.then(res => {
-			dispatch({
-				type: USER_LOADING,
-				payload: res.data
-			});
-		})
-		.catch(err => {
-			dispatch(returnErrors(err.response.data, err.response.status));
-			dispatch({
-				type: AUTH_ERROR
-			});
-		});
+  axios
+    .get("/api/auth/user", config)
+    .then(res => {
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({
+        type: AUTH_ERROR
+      });
+    });
+};
+
+// login user
+export const login = (username, password) => dispatch => {
+  // send headers with request
+  const config = {
+    headers: {
+      "content-type": "application/json"
+    }
+  };
+
+  // request body
+  const body = JSON.stringify({
+    username,
+    password
+  });
+
+  axios
+    .post("/api/auth/login", body, config)
+    .then(res => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({
+        type: LOGIN_FAIL
+      });
+    });
 };
