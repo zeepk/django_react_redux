bc88a7f301b4e3a3d2452f2212d95842f4a448cc
unknown <mhughes.ipod@gmail.com>
2020-03-14 16:34:18 -0700

    changed wsgi

--- leadmanager/leadmanager/wsgi.py
+++ leadmanager/leadmanager/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-project_home = u'/leadmanager'
+# project_home = u'/leadmanager'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leadmanager.settings')
 
