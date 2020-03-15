9007f0d6affdece6c057cf23578121dde7fd83b9
unknown <mhughes.ipod@gmail.com>
2020-03-14 19:06:40 -0700

    contrib sites

--- leadmanagerproject/leadmanager/settings.py
+++ leadmanagerproject/leadmanager/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'whitenoise.runserver_nostatic',
     'leadmanagerproject.leads',
     'rest_framework',
