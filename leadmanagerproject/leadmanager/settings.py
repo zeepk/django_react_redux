0069ed4f15bf3d700c046d311ba70588d6bb6d56
unknown <mhughes.ipod@gmail.com>
2020-03-14 18:33:11 -0700

    no leadmanager

--- leadmanagerproject/leadmanager/settings.py
+++ leadmanagerproject/leadmanager/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
-    'leadmanager',
     'leadmanagerproject.leads',
     'leadmanagerproject.leadmanager',
     'rest_framework',
