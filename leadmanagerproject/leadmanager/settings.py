6582335ceb4dc89708e34f75cbbd1d9f65d12bda
unknown <mhughes.ipod@gmail.com>
2020-03-14 18:42:34 -0700

    frontend

--- leadmanagerproject/leadmanager/settings.py
+++ leadmanagerproject/leadmanager/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'leadmanagerproject.leads',
     'rest_framework',
-    'leadmanagerproject.frontend',
+    'frontend',
     'knox',
     'leadmanagerproject.accounts'
 ]
