aca03c6f21e80225c57f608c8ec861e8ca3f057d
unknown <mhughes.ipod@gmail.com>
2020-03-14 20:04:49 -0700

    urls

--- leadmanagerproject/leadmanager/urls.py
+++ leadmanagerproject/leadmanager/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('leadmanagerproject.leads.urls')),
     path('', include('leadmanagerproject.frontend.urls')),
+    path('', include('leadmanagerproject.leads.urls')),
     path('', include('leadmanagerproject.accounts.urls')),
 ]
